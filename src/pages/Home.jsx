b329a32476e5107c1d6f062a609cedf352c9491e
fabetabilo <fab.tabilo@duocuc.cl>
2026-08-30 22:43:21 -0400

    Add: componente carrusel instituciones

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,18 @@
 import Carousel from '../components/ui/Carousel/Carousel'
 import EventsCarousel from '../components/ui/Carousel/EventsCarousel'
 import BoldBanner from '../components/ui/Banner/BoldBanner'
+import InstCarousel from '../components/ui/Carousel/InstCarousel'
 import { nextEvents } from '../data/nextEvents'
 import { heroSlides } from '../data/heroSlides'
+import { institutions } from '../data/institutions'
 
 export default function Home() {
   return (
     <>
       <Carousel slides={heroSlides} />
       <EventsCarousel events={nextEvents} />
-      <BoldBanner outlineText="36+INSTITUCIONES" solidText="Un solo equipo"/>
+      <BoldBanner outlineText="36+INSTITUCIONES" solidText="Un solo equipo" />
+      <InstCarousel institutions={institutions} />
     </>
   )
 }
